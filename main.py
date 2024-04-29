94d02fe5d97e4e5a54a37a3c4dfb32209805ca21
Vansh Shah <75292413+VanshShah1@users.noreply.github.com>
2024-04-29 23:17:43 +0530

    ds cs,dvv

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 import flet as ft
 
 def main(page: ft.Page):
-    page.add(ft.SafeArea(ft.Text("Hello, Flet!")))
+    page.bgcolor="#d3d3d3"
 
+    row1=ft.Row(controls=[
+        ft.Container(content=ft.Text('helo wold'),
+                         bgcolor="#2b2b2b",
+                         width=620,
+                         height=600),
+        ft.Container(content=ft.Text('helo wold'),
+                             bgcolor="black",
+                             width=800,
+                             height=600)
+        ]       
+    )
+    row2=ft.Row(controls=[
+        ft.Container(content=ft.Text('helo wold'),
+                         bgcolor="white",
+                         width=620,
+                         height=300),
+        ft.Container(content=ft.Text('helo wold'),
+                             bgcolor="black",
+                             width=800,
+                             height=300)
+        ]       
+    )
+    
+
+    page.add(row1, row2)
 
 ft.app(main)
